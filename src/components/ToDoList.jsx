cebeb76f65307c6b65d5f1629b693bf3922268cd
Ania <anna.ananicz@gmail.com>
2020-08-25 11:49:14 +0200

    Adding and removing items working

---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+//
+//
+// function ToDoList() {
+//   return (
+//     <div className="toDoList">
+//
+//
+//     </div>
+//   );
+// }
+//
+// export default ToDoList;
